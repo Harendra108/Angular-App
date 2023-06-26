c0e95204e319d368a7fe874f7999b4f43ae7ae10
Singh <hpsingh@luxoft.com>
2023-06-26 20:26:54 +0200

    recipe-list changes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Tomato Pie", "Recipe to cook Tomato Pie", "https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9")
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
 
 }
